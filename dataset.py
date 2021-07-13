a897ecc05a5cf97a6f3f4267148615a964655a4f
jaejunL <jjlee0721@snu.ac.kr>
2021-07-13 05:55:55 +0000

    first commit

---
+++ dataset.py
@@ -0,0 +1,159 @@
+import os
+import cv2
+import argparse
+import librosa
+import numpy as np
+
+import torch
+import torchaudio
+import torchaudio.transforms as T
+from torch.utils import data
+from torchvision import transforms
+from torch.utils.data import Dataset
+
+import matplotlib
+import matplotlib.pyplot as plt
+from IPython.display import Audio, display
+
+import easydict
+from sklearn.model_selection import train_test_split
+ALLMASKTYPES = ['time_masking', 'bbox', 'free_form']
+
+
+class InpaintDataset(Dataset):
+    def __init__(self, opt, split):
+        self.opt = opt
+        self.split = split
+        
+        self.get_list()
+        
+    def __getitem__(self, index):
+        audio = self.get_audio(index)
+        complex_spec = self.get_comlex_spectrogram(audio)
+
+        if self.split == 'TRAIN':
+            if self.opt.mask_type == 'time_masking':
+                mask = self.time_mask()
+            if self.opt.mask_type == 'bbox':
+                mask = self.bbox2mask()
+            if self.opt.mask_type == 'free_form':
+                mask = self.random_ff_mask()
+        elif self.split in ['VALID', 'TEST']:
+            if self.opt.mask_type == 'time_masking':
+                masks_dir = '../split/fixedmask_time'
+                mask = np.load(os.path.join(masks_dir, str(index)))
+            if self.opt.mask_type == 'bbox':
+                masks_dir = '../split/fixedmask_bbox'
+                mask = np.load(os.path.join(masks_dir, str(index)))
+            if self.opt.mask_type == 'free_form':
+                masks_dir = '../split/fixedmask_freeform'
+                mask = np.load(os.path.join(masks_dir, str(index)))
+
+        complex_spec = complex_spec.squeeze(0).permute(2, 0, 1).contiguous()
+        mask = torch.from_numpy(mask.astype(np.float32)).contiguous()
+
+        return complex_spec, mask
+    
+    def get_list(self):
+        margs_trainvalid = np.loadtxt('../split/margs_trainvalid.txt', delimiter=',', dtype=str)
+        margs_test = np.loadtxt('../split/margs_test.txt', delimiter=',', dtype=str)
+        nuss_trainvalid = np.loadtxt('../split/nuss_trainvalid.txt', delimiter=',', dtype=str)
+        nuss_test = np.loadtxt('../split/nuss_test.txt', delimiter=',', dtype=str)
+        vocals_trainvalid = np.loadtxt('../split/vocals_trainvalid.txt', delimiter=',', dtype=str)
+        vocals_test = np.loadtxt('../split/vocals_test.txt', delimiter=',', dtype=str)
+
+        margs_train, margs_valid = train_test_split(margs_trainvalid, test_size=0.1, shuffle=False, random_state=21)
+        nuss_train, nuss_valid = train_test_split(nuss_trainvalid, test_size=0.1, shuffle=False, random_state=21)
+        vocals_train, vocals_valid = train_test_split(vocals_trainvalid, test_size=0.1, shuffle=False, random_state=21)
+
+        if self.split == 'TRAIN':
+            train_list = list(margs_train) + list(nuss_train) + list(vocals_train)
+            self.fl = train_list
+        elif self.split == 'VALID':
+            valid_list = list(margs_valid) + list(nuss_valid) + list(vocals_valid)
+            self.fl = valid_list
+        elif self.split == 'TEST':
+            test_list = list(margs_test) + list(nuss_valid) + list(vocals_test)
+            self.fl = test_list
+    
+    def get_audio(self, index):
+        fn = self.fl[index]
+        audio_path = os.path.join(self.opt.data_dir, fn)
+        audio, sr = torchaudio.load(audio_path)
+        random_idx = int(np.floor(np.random.random(1) * (audio.shape[-1]-self.opt.input_length)))
+        audio = audio[:,random_idx:random_idx+self.opt.input_length]
+        audio_pad = torch.nn.functional.pad(audio, (0, 44100*5-audio.shape[-1]), mode='constant', value=0)
+        return audio_pad
+
+    def get_comlex_spectrogram(self,waveform, n_fft = 1024, win_len = 1024, hop_len = 512, power=None):
+        spectrogram = T.Spectrogram(
+        n_fft=n_fft,
+        win_length=win_len,
+        hop_length=hop_len,
+        center=True,
+        pad_mode="reflect",
+        power=power,
+        return_complex=0,
+        )
+        return spectrogram(waveform)
+
+    def random_bbox(self):
+        max_freq_ix = self.opt.image_height - self.opt.bbox_shape
+        max_frame_ix = self.opt.image_width - self.opt.bbox_shape
+
+        box_freq_ix = np.random.randint(max_freq_ix)
+        box_frame_ix = np.random.randint(max_frame_ix)
+
+        return (box_freq_ix, box_frame_ix, self.opt.bbox_shape, self.opt.bbox_shape)
+
+    def time_mask(self):
+        mask_width = np.random.randint(low=8, high=87)
+        max_frame_ix = self.opt.image_width - mask_width
+        t = np.random.randint(max_frame_ix)
+        mask = np.zeros((self.opt.image_height, self.opt.image_width))
+        mask[:,t:t+mask_width] = 1
+        return mask.reshape((1, ) + mask.shape).astype(np.float32)
+
+    def bbox2mask(self):
+        bboxs = []
+        times = np.random.randint(8)
+        for i in range(times):
+            bbox = self.random_bbox(self.opt)
+            bboxs.append(bbox)
+        mask = np.zeros((self.opt.image_height, self.opt.image_width), np.float32)
+        for bbox in bboxs:
+            h = int(bbox[2] * 0.1) + np.random.randint(int(bbox[2] * 0.2 + 1))
+            w = int(bbox[3] * 0.1) + np.random.randint(int(bbox[3] * 0.2) + 1)
+            mask[(bbox[0] + h) : (bbox[0] + bbox[2] - h), (bbox[1] + w) : (bbox[1] + bbox[3] - w)] = 1.
+        return mask.reshape((1, ) + mask.shape).astype(np.float32)
+
+    def random_ff_mask(self):
+        """Generate a random free form mask with configuration.
+        Args:
+            config: Config should have configuration including IMG_SHAPES,
+                VERTICAL_MARGIN, HEIGHT, HORIZONTAL_MARGIN, WIDTH.
+        Returns:
+            tuple: (top, left, height, width)
+        """
+        mask = np.zeros((self.opt.image_height, self.opt.image_width), np.float32)
+        max_angle = 4
+        max_len = 200
+        max_width = 20
+        times = np.random.randint(7)
+        for i in range(times):
+            start_x = np.random.randint(self.opt.image_width)
+            start_y = np.random.randint(self.opt.image_height)
+            for j in range(1 + np.random.randint(5)):
+                angle = 0.01 + np.random.randint(max_angle)
+                if i % 2 == 0:
+                    angle = 2 * 3.1415926 - angle
+                length = 10 + np.random.randint(max_len)
+                brush_w = 5 + np.random.randint(max_width)
+                end_x = (start_x + length * np.sin(angle)).astype(np.int32)
+                end_y = (start_y + length * np.cos(angle)).astype(np.int32)
+                cv2.line(mask, (start_y, start_x), (end_y, end_x), 1.0, brush_w)
+                start_x, start_y = end_x, end_y
+        return mask.reshape((1, ) + mask.shape).astype(np.float32)
+
+    def __len__(self):
+        return len(self.fl)
---
+++ train.py
@@ -0,0 +1,81 @@
+import argparse
+import os
+
+if __name__ == "__main__":
+
+    # ----------------------------------------
+    #        Initialize the parameters
+    # ----------------------------------------
+    parser = argparse.ArgumentParser()
+    # General parameters
+    parser.add_argument('--save_path', type = str, default = '/data2/personal/jaejun/inpainting/results/210714/timemask', help = 'saving path that is a folder')
+    parser.add_argument('--sample_path', type = str, default = '/data2/personal/jaejun/inpainting/results/210714/timemask/samples', help = 'training samples path that is a folder')
+    parser.add_argument('--data_dir', type = str, default = '../dataset', help = 'dataset directory')
+    parser.add_argument('--gan_type', type = str, default = 'WGAN', help = 'the type of GAN for training')
+    parser.add_argument('--multi_gpu', type = bool, default = True, help = 'nn.Parallel needs or not')
+    parser.add_argument('--gpu_ids', type = str, default = "0, 1", help = 'gpu ids: e.g. 0  0,1,2, 0,2. use -1 for CPU')
+    parser.add_argument('--cudnn_benchmark', type = bool, default = True, help = 'True for unchanged input data type')
+    parser.add_argument('--checkpoint_interval', type = int, default = 1, help = 'interval between model checkpoints')
+    parser.add_argument('--load_name', type = str, default = '', help = 'load model name')
+    # Training parameters
+    parser.add_argument('--epochs', type = int, default = 60, help = 'number of epochs of training')
+    parser.add_argument('--batch_size', type = int, default = 4, help = 'size of the batches')
+    parser.add_argument('--lr_g', type = float, default = 1e-4, help = 'Adam: learning rate')
+    parser.add_argument('--lr_d', type = float, default = 4e-4, help = 'Adam: learning rate')
+    parser.add_argument('--b1', type = float, default = 0.5, help = 'Adam: beta 1')
+    parser.add_argument('--b2', type = float, default = 0.999, help = 'Adam: beta 2')
+    parser.add_argument('--weight_decay', type = float, default = 0, help = 'Adam: weight decay')
+    parser.add_argument('--lr_decrease_epoch', type = int, default = 5, help = 'lr decrease at certain epoch and its multiple')
+    parser.add_argument('--lr_decrease_factor', type = float, default = 0.5, help = 'lr decrease factor, for classification default 0.1')
+    parser.add_argument('--lambda_l1', type = float, default = 100, help = 'the parameter of L1Loss')
+    parser.add_argument('--lambda_perceptual', type = float, default = 10, help = 'the parameter of FML1Loss (perceptual loss)')
+    parser.add_argument('--lambda_gan', type = float, default = 1, help = 'the parameter of valid loss of AdaReconL1Loss; 0 is recommended')
+    parser.add_argument('--num_workers', type = int, default = 8, help = 'number of cpu threads to use during batch generation')
+    # Network parameters
+    parser.add_argument('--in_channels', type = int, default = 3, help = 'input real&complex spec + 1 channel mask')
+    parser.add_argument('--out_channels', type = int, default = 2, help = 'output real&complex spec')
+    parser.add_argument('--latent_channels', type = int, default = 64, help = 'latent channels')
+    parser.add_argument('--pad_type', type = str, default = 'zero', help = 'the padding type')
+    parser.add_argument('--activation', type = str, default = 'lrelu', help = 'the activation type')
+    parser.add_argument('--norm', type = str, default = 'in', help = 'normalization type')
+    parser.add_argument('--init_type', type = str, default = 'xavier', help = 'the initialization type')
+    parser.add_argument('--init_gain', type = float, default = 0.02, help = 'the initialization gain')
+    # Dataset parameters
+    parser.add_argument('--baseroot', type = str, default = "C:\\Users\\yzzha\\Desktop\\dataset\\ILSVRC2012_val_256", help = 'the training folder')
+    parser.add_argument('--mask_type', type = str, default = 'time_masking', help = 'mask type')
+    parser.add_argument('--image_height', type = int, default = 513, help = 'height of image')
+    parser.add_argument('--image_width', type = int, default = 431, help = 'width of image')
+    parser.add_argument('--input_length', type = int, default = 220500, help = 'input length (sample)')
+
+    parser.add_argument('--margin', type = int, default = 10, help = 'margin of image')
+    # parser.add_argument('--mask_num', type = int, default = 15, help = 'number of mask')
+    parser.add_argument('--bbox_shape', type = int, default = 120, help = 'margin of image for bbox mask')
+    # parser.add_argument('--max_angle', type = int, default = 4, help = 'parameter of angle for free form mask')
+    # parser.add_argument('--max_len', type = int, default = 40, help = 'parameter of length for free form mask')
+    # parser.add_argument('--max_width', type = int, default = 10, help = 'parameter of width for free form mask')
+    opt = parser.parse_args()
+    print(opt)
+    
+    '''
+    # ----------------------------------------
+    #       Choose CUDA visible devices
+    # ----------------------------------------
+    if opt.multi_gpu == True:
+        os.environ["CUDA_VISIBLE_DEVICES"] = opt.gpu_ids
+    else:
+        os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+    '''
+    
+    # Enter main function
+    import trainer
+    import wandb
+    wandb.init()
+    wandb.config.update(opt)
+
+    os.environ["CUDA_VISIBLE_DEVICES"] = '0, 1'
+
+    if opt.gan_type == 'WGAN':
+        trainer.WGAN_trainer(opt)
+    if opt.gan_type == 'LSGAN':
+        trainer.LSGAN_trainer(opt)
+    
